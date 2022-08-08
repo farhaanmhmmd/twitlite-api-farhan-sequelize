877394387b6d3d610dcd9feed9fced71f0b97358
farhaanmhmmd <farhanmhmmd96@gmail.com>
2022-08-08 17:16:38 +0700

    Create Token Expired Page

--- src/routers/users/get.user.js
+++ src/routers/users/get.user.js
@@ -44,7 +44,60 @@ const verifyUserController = async (req, res, next) => {
           }
         );
       } else {
-        res.send("Token Expired");
+        res.send(`<!DOCTYPE html>
+        <html>
+          <head>
+            <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+          </head>
+          <style>
+            body {
+              text-align: center;
+              padding: 120px 0;
+              background: #EBF0F5;
+            }
+            h1 {
+              color: #88B04B;
+              font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+              font-weight: 900;
+              font-size: 30px;
+              margin-bottom: 10px;
+            }
+            h2 {
+              color: #FF0000;
+              font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+              font-weight: 900;
+              font-size: 30px;
+              margin-bottom: 10px;
+            }
+            p {
+              color: #404F5E;
+              font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+              font-size:20px;
+              margin-bottom: 10px;
+            }
+            i {
+               color: #9ABC66;
+              font-size: 100px;
+              line-height: 200px;
+              margin-left:-15px;
+            } 
+            .card {
+            background: white;
+            padding: 60px;
+            border-radius: 4px;
+            box-shadow: 0 2px 3px #C8D0D8;
+            display: inline-block;
+            margin: 0 auto;
+          }
+          </style>
+          <body>
+          <div class="card">
+            <h2>Verification Failed</h2> 
+            <p>Your token has expired. The old token expires as soon as the new token is sent.</p>
+            <p>Go check your newest email verification from Twitlite.</p>
+            </div>
+          </body>
+        </html>`);
       }
     }
 
